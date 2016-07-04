785434e6604ebee8d5d9b805badb8c004b441818
guyuECMAScript <820571982@qq.com>
2016-07-04 21:04:14 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -24,6 +24,22 @@ var COMMON = {
             }, d * 1000);
         }, duration);
     },
+    showToast2: function (msg, duration) {
+        duration = isNaN(duration) ? 2000 : duration;
+        var m = document.createElement('div');
+        m.innerHTML = msg;
+        m.setAttribute('class', 'toastDiv2');
+        document.body.appendChild(m);
+        setTimeout(function () {
+            var d = 0.5;
+            m.style.webkitTransition = '-webkit-transform ' + d
+                + 's ease-in, opacity ' + d + 's ease-in';
+            m.style.opacity = '0';
+            setTimeout(function () {
+                document.body.removeChild(m)
+            }, d * 1000);
+        }, duration);
+    },
     checkPwd: function (id) {
         var pwd = document.getElementById(id).value;
 
@@ -56,13 +72,13 @@ var COMMON = {
         var acct = document.getElementById(id).value;
 
         if (!acct) {
-            this.showToast("邮箱不能为空");
+            this.showToast2("邮箱不能为空");
             return false;
         }
 
         var emailReg = /^([a-zA-Z-z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})$/;
         if (!emailReg.test(acct)) {
-            this.showToast("您输入的邮箱有误,请重新核对后再输入!");
+            this.showToast2("您输入的邮箱有误,请重新核对后再输入!");
             return false;
         }
 
@@ -73,7 +89,7 @@ var COMMON = {
         var name = document.getElementById(id).value;
 
         if (!name) {
-            this.showToast("姓名不能为空");
+            this.showToast2("姓名不能为空");
             return false;
         }
         return true;
@@ -83,12 +99,12 @@ var COMMON = {
         var phone = document.getElementById(id).value;
 
         if (!phone) {
-            this.showToast("电话不能为空");
+            this.showToast2("电话不能为空");
             return false;
         }
         var phoneReg = /^(?:13\d|14\d|15\d|17\d|18\d)\d{5}(\d{3}|\*{3})$/;
         if (!phoneReg.test(phone)) {
-            this.showToast("您输入的电话号码有误,请重新核对后再输入!");
+            this.showToast2("您输入的电话号码有误,请重新核对后再输入!");
             return false;
         }
         return true;
